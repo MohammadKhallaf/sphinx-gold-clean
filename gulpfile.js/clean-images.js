14b4882b7d568886186caf1825ce44bdfa1b9e4b
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2022-07-26 16:57:20 +0200

    export clean images task

--- gulpfile.js/clean-images.js
+++ gulpfile.js/clean-images.js
@@ -1,23 +1,23 @@
-function (cb) {
-    lessonsArr.forEach((nameLesson) => {
-      console.log(nameLesson);
-      gulp
-        .src([
-          `${nameLesson}/resources/images/**/*`,
-          `${nameLesson}/assets/**/*.css`,
-          `${nameLesson}/*.html`,
-        ])
-        .pipe(plumber())
-        .pipe(
-          unusedImages({
-            log: true,
-            delete: true,
-          })
-        )
-        .on("end", () =>
-          console.log("-".repeat(10) + nameLesson + "-".repeat(10))
-        )
-        .pipe(plumber.stop());
-    });
-    cb();
-  }
+module.exports = function (cb) {
+  lessonsArr.forEach((nameLesson) => {
+    console.log(nameLesson);
+    gulp
+      .src([
+        `${nameLesson}/resources/images/**/*`,
+        `${nameLesson}/assets/**/*.css`,
+        `${nameLesson}/*.html`,
+      ])
+      .pipe(plumber())
+      .pipe(
+        unusedImages({
+          log: true,
+          delete: true,
+        })
+      )
+      .on("end", () =>
+        console.log("-".repeat(10) + nameLesson + "-".repeat(10))
+      )
+      .pipe(plumber.stop());
+  });
+  cb();
+};
--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,8 +1,9 @@
 const cleanhtml = require("./clean-html");
 const cleancss = require("./clean-css");
 const imagemin = require("./image-min");
-
+const imageclean = require("./clean-images");
 
 exports.cleanhtml = cleanhtml;
 exports.cleancss = cleancss;
 exports.imagemin = imagemin;
+exports.imageclean = imageclean;
