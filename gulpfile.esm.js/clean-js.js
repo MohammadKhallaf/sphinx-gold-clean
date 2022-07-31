52bbc7fe3dbd5ade86d52e84a4da831bca26adea
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2022-07-31 16:42:18 +0200

    add plumber to js cleaning

--- gulpfile.esm.js/clean-js.js
+++ gulpfile.esm.js/clean-js.js
@@ -4,13 +4,16 @@ const minify = require("gulp-minify");
 var uglify = require("gulp-uglify");
 const gulp = require("gulp");
 const del = require("del");
+const gulpPlumber = require("gulp-plumber");
 
 module.exports = function (cb) {
   // del("src/**/*-min.js");
   gulp
     .src("src/**/*.js")
+    .pipe(gulpPlumber())
     .pipe(uglify())
     .pipe(beautify())
+    .pipe(gulpPlumber.stop())
     .pipe(gulp.dest("src"));
   cb();
 };
